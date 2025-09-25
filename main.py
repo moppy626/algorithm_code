e9f5291b80ed13a521302c13c8fd105d869d2ef6
moppy626 <m_himenezumi@yahoo.co.jp>
2025-09-25 16:40:47 +0900

    とりあえず空いている場所を返す

--- main.py
+++ main.py
@@ -9,4 +9,28 @@ class MyAI(Alg3D):
         player: int, # 先手(黒):1 後手(白):2
         last_move: Tuple[int, int, int] # 直前に置かれた場所(x, y, z)
     ) -> Tuple[int, int]:
-        # ここにアルゴリズムを書く
+        # 空いている座標を取得
+        moves = self.get_available_moves(board)
+        # とりあえず最初の候補を返す
+        if moves:
+            return moves[0][:2]  # (x, y) だけ返す
+        else:
+            raise ValueError("置ける場所がありません。")
+
+    def get_available_moves(
+        self,
+        board: List[List[List[int]]]
+    ) -> List[Tuple[int, int, int]]:
+        """ 置ける場所(最下段の空き)を返す """
+        size_x = len(board)
+        size_y = len(board[0])
+        size_z = len(board[0][0])
+
+        available = []
+        for x in range(size_x):
+            for y in range(size_y):
+                for z in range(size_z):
+                    if board[x][y][z] == 0:  # 空き
+                        available.append((x, y, z))
+                        break  # 一番下の空きだけで良いのでbreak
+        return available
