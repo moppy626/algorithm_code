b5d84cf729ab55d2503a51fec970f6b0569ff0b3
moppy626 <m_himenezumi@yahoo.co.jp>
2025-09-25 18:38:39 +0900

    エラーを修正

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from local_driver import Alg3D, Board  # ローカル検証用
 class MyAI(Alg3D):
 	def get_best_move(self, score_board: List[List[int]]) -> Tuple[int, int]:
 		# 4x4 の二次元配列から最大の点数を持つ (x, y) を返す
-		best_score = 0
+		best_score = -10**9
 		best_move = (0, 0)
 		for y in range(4):
 			for x in range(4):
@@ -24,27 +24,25 @@ class MyAI(Alg3D):
 
 		score_board = [[0]*4 for _ in range(4)]
 
-		#すでに埋まっている箇所に-100を設定
-		print("=== 盤面探索開始 ===")
-		for y in range(4):
-			for x in range(4):
-				for z in range(4):
-					# 各 (x, y) の最下段セルを出力して確認
-					print(f"checking (x={x}, y={y}, z={z}): value={board[z][y][x]}")
-					if board[z][y][x] == 0:
-						print(f"--> 選択: (x={x}, y={y})")
-						score_board[x][y] = -100
-
 		# 端っこが空いていたら重めに配点
 		if board[0][0][0] == 0:
 			score_board[0][0]+=10
 		if board[0][3][0] == 0:
-			score_board[0][3]+=10
+			score_board[3][0]+=10
 		if board[0][3][3] == 0:
 			score_board[3][3]+=10
 		if board[0][0][3] == 0:
-			score_board[3][0]+=10
+			score_board[0][3]+=10
 
+		#すでに埋まっている箇所に-10**9を設定
+		print("=== 盤面探索開始 ===")
+		for y in range(4):
+			for x in range(4):
+					if board[3][y][x] > 0:
+						print(f"--> 選択: (x={x}, y={y})")
+						score_board[y][x] = -10**9
+					# 各 (x, y) の最下段セルを出力して確認
+					#print(f"checking (x={x}, y={y}, z={z}): value={board[z][y][x]}")
 		x, y = self.get_best_move(score_board)
 		return (x, y)
 
